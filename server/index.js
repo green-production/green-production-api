62415a15855a36b7e5b51434501e0abcddaf381a
green-production <produce.recycle@gmail.com>
2021-07-21 19:43:22 +0530

    modified gitignore and index.js

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
 // Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
+const fastify = require('fastify')({
+  logger: true
+})
 
 // Declare a route
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
 })
 
 // Run the server!
-const start = async () => {
-  try {
-    await fastify.listen(3000)
-  } catch (err) {
+fastify.listen(3000, function (err, address) {
+  if (err) {
     fastify.log.error(err)
     process.exit(1)
   }
-}
-start()
+  fastify.log.info(`server listening on ${address}`)
+})
