383dc0db62cd68cb4d26403bbbcf70346a9ff51a
green-production <produce.recycle@gmail.com>
2021-07-22 11:50:58 +0530

    Moved cloudant functions to util.js

--- server/index.js
+++ server/index.js
@@ -3,14 +3,17 @@ const fastify = require('fastify')({
   logger: true
 })
 
+const utils = require('./utils/utils');
+
 // Declare a route
 fastify.get('/', function (request, reply) {
 
-  getDocumentFromDB();
+  var data = utils.getDocumentFromDB();
+  console.log('Get-DB-Data', data);
   
   //createCloudantDB();
 
-  reply.send({ hello: 'world' })
+  reply.send({ data: data })
 })
 
 // Run the server!
@@ -21,59 +24,3 @@ fastify.listen(3000, function (err, address) {
   }
   fastify.log.info(`server listening on ${address}`)
 })
-
-
-function createCloudantDB() {
-  const vcap = require('../server/config/vcap-local.json');
-
-  // Load the Cloudant library.
-  var Cloudant = require('@cloudant/cloudant');
-
-  // Get account details from environment variables
-  var url = vcap.services.cloudantNoSQLDB.credentials.url;
-  var username = vcap.services.cloudantNoSQLDB.credentials.username;
-  var password = vcap.services.cloudantNoSQLDB.credentials.password;
-
-  // Initialize the library with url and credentials.
-  var cloudant = Cloudant({ url: url, username: username, password: password });
-
-  var uniqueUserID = Math.random().toString(26).slice(2);
-
-  cloudant.db.create('green-production').then(() => {
-    cloudant.use('green-production').insert({ user_ID: uniqueUserID }, 'Users').then((data) => {
-      console.log(data);
-    });
-  }).catch((err) => {
-    console.log(err);
-  });
-}
-
-function getDocumentFromDB() {
-  const vcap = require('../server/config/vcap-local.json');
-
-  // Load the Cloudant library.
-  var Cloudant = require('@cloudant/cloudant');
-
-  // Get account details from environment variables
-  var url = vcap.services.cloudantNoSQLDB.credentials.url;
-  var username = vcap.services.cloudantNoSQLDB.credentials.username;
-  var password = vcap.services.cloudantNoSQLDB.credentials.password;
-
-  // Initialize the library with url and credentials.
-  var cloudant = Cloudant({ url: url, username: username, password: password });
-
-  var mydb = cloudant.db.use('green-production');
-  
-  const myData = mydb.get('Users', function(err, data) {
-    console.log('data',data);   
-  });
-
-  console.log('myData',myData);  
-
-  if(myData != null) {
-    return true;
-  }      
-  else {
-    return false;
-  }
-}
--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,21 +1,40 @@
-const Cloudant = require('@cloudant/cloudant');
-
+// Load the Cloudant service config.
 const vcap = require('../config/vcap-local.json');
+  
+// Load the Cloudant library.
+var Cloudant = require('@cloudant/cloudant');
 
-function dbCloudantConnect() {
-    return new Promise((resolve, reject) => {
-        Cloudant({ 
-            url: vcap.services.cloudantNoSQLDB.credentials.url
-        }, ((err, cloudant) => {
-            if (err) {
-                logger.error('Connect failure: ' + err.message + ' for Cloudant DB: ' +
-                    appSettings.cloudant_db_name);
-                reject(err);
-            } else {
-                let db = cloudant.use(appSettings.cloudant_db_name);
-                logger.info('Connect success! Connected to DB: ' + appSettings.cloudant_db_name);
-                resolve(db);
-            }
-        }));
-    });
+// Get account details from environment variables
+var url = vcap.services.cloudantNoSQLDB.credentials.url;
+var username = vcap.services.cloudantNoSQLDB.credentials.username;
+var password = vcap.services.cloudantNoSQLDB.credentials.password;
+
+// Initialize the library with url and credentials.
+var cloudant = Cloudant({ url: url, username: username, password: password });
+
+module.exports = {
+    createCloudantDB: function createCloudantDB() {  
+        var uniqueUserID = Math.random().toString(26).slice(2);
+      
+        cloudant.db.create('green-production').then(() => {
+          cloudant.use('green-production').insert({ user_ID: uniqueUserID }, 'Users').then((data) => {
+            console.log(data);
+          });
+        }).catch((err) => {
+          console.log(err);
+        });
+    },
+      
+    getDocumentFromDB: function getDocumentFromDB() {  
+        var mydb = cloudant.db.use('green-production');
+        //var myData;
+    
+        return mydb.get('Users', function(err, data) {
+          console.log('data',data);  
+          //myData = JSON.stringify(data); 
+        });
+
+        //return myData;
+    }
+
 }
