86f27bf9435863fd1be285697e2ebf213a4e6c13
green-production <produce.recycle@gmail.com>
2021-07-24 16:04:55 +0530

    listen port modifictaion

--- server/index.js
+++ server/index.js
@@ -353,7 +353,7 @@ fastify.post('/get-seller-listings', async function (request, reply) {
 
 
 // Run the server!
-fastify.listen(process.env.PORT || 3000, function (err, address) {
+fastify.listen(process.env.PORT || 3000, '0.0.0.0', function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
