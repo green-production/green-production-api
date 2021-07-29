05a9d364d8cf6d9cc6e003c5bb3b191a8180bf65
green-production <produce.recycle@gmail.com>
2021-07-29 22:47:22 +0530

    file upload api

---
+++ server/middleware/upload-parser.js
@@ -0,0 +1,15 @@
+
+
+const upload = multer({
+    limits: {
+        fileSize: 1.049e+6
+    },
+    fileFilter(req, file, cb) {
+        // if(!file.originalname.endsWith('.pdf')) { //here we are using endswith method to check if a single file those charactes at the end
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) { //here match uses regex to chec extentions of multiple files which we are allowing
+            cb(new Error('Please upload a JPG/JPEG/PNG file'))
+        }
+
+        cb(undefined, true)
+    }
+})
